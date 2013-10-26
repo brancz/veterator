29fab3ac041d0f0e441c69d7f4d920dd995c990b
flower-pot <flowerpot@pondati.net>
2013-10-26 18:00:09 +0200

    Rails base added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OShome::Application.config.secret_key_base = '03907d3c07096628b27f8048ce7d42f2dd2baf548be7de10b6e1bf861d0c5b53800927364efe615372fec34e2f9cf1ca0e2f7e471ae1b83abef769b76b94998e'
