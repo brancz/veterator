7be0781c86a7a44a60a6bbbf93f295e75f1714ed
flower-pot <flowerpot@pondati.net>
2013-12-06 14:35:22 +0100

    add statistics to sensor index

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -13,4 +13,27 @@ class Sensor < ActiveRecord::Base
 
   validates :user_id,
     presence: true
+
+  def statistics(since = 1.day.ago)
+    arecords = records.where('created_at > ?', since).order('value ASC')
+    statistics_data = Hash.new
+    statistics_data[:min] = 0
+    statistics_data[:max] = 0
+    statistics_data[:avg] = 0
+    statistics_data[:q1] = 0
+    statistics_data[:q2] = 0
+    statistics_data[:q3] = 0
+    statistics_data[:iqr] = 0
+    if arecords.count > 0
+      statistics_data[:min] = arecords.minimum('value')
+      statistics_data[:max] = arecords.maximum('value')
+      statistics_data[:avg] = arecords.average('value')
+      # calcultates the first through third quartile and the interquartile range
+      statistics_data[:q1] = arecords[(arecords.count/4).to_i].value
+      statistics_data[:q2] = arecords[(arecords.count/2).to_i].value
+      statistics_data[:q3] = arecords[((arecords.count/4)*3).to_i].value
+      statistics_data[:iqr] = statistics_data[:q3] - statistics_data[:q1]
+    end
+    statistics_data
+  end
 end
