524bae4f8a31f891c2e44a348464e1525d680cd1
flower-pot <flowerpot@pondati.net>
2013-12-22 08:57:23 +0100

    add csv format to records

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,4 +6,14 @@ class Record < ActiveRecord::Base
 
   validates :sensor_id,
     presence: true
+
+  def self.to_csv(options = {})
+    require 'csv'
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |record|
+        csv << record.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
