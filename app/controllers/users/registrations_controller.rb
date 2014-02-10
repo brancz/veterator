5dce5895d0aea7d93137f0fd7cb53d47cee0c8e7
flower-pot <flowerpot@pondati.net>
2014-02-10 20:38:25 +0100

    fix permission error
    no default role was given to a created user thus a user had no
    authorization

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,5 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	def after_inactive_sign_up_path_for(resource)
 		'/users/sign_in'
 	end
+
+	def sign_up_params
+		result = super
+		result[:roles] = [Role.find_by_name("user")]
+		result
+	end
 end
 
