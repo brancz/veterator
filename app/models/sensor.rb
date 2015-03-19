1572a8b2d69b2787e55f5f471cb83b31be40da82
flower-pot <fbranczyk@gmail.com>
2015-03-19 12:18:51 +0100

    delete records of sensors when deleting sensor

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,6 +1,6 @@
 class Sensor < ActiveRecord::Base
   belongs_to :user
-  has_many :records
+  has_many :records, dependent: :delete_all
   enum chart_type: ['linear', 'linear-closed', 'step', 'step-before',
                     'step-after', 'basis', 'basis-open', 'basis-closed',
                     'bundle', 'cardinal', 'cardinal-open', 'cardinal-closed',
