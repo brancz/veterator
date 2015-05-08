193912d10303b6120f57857f77021fae1cd97a66
flower-pot <fbranczyk@gmail.com>
2015-05-08 17:08:41 +0200

    add scopes to calculate different averages

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,4 +4,10 @@ class Record < ActiveRecord::Base
   enum granularity: [ :finest, :daily, :monthly, :yearly ]
 
   validates :value, presence: true
+
+  default_scope { where(granularity: granularities[:finest]) }
+  scope :avg, -> { average(:value) }
+  scope :avg_on_day,   ->(day)   { where(created_at: day.beginning_of_day..day.end_of_day).avg         }
+  scope :avg_in_month, ->(month) { where(created_at: month.beginning_of_month..month.end_of_month).avg }
+  scope :avg_in_year,  ->(year)  { where(created_at: year.beginning_of_year..year.end_of_year).avg     }
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,35 +1,154 @@
-describe 'Record#valid?' do
-  subject { @sensor.records.new(value: value) }
+RSpec.describe Record do
   before :each do
-    @sensor = Sensor.new(
+    @sensor = Sensor.create(
       title: 'Test Title',
       description: 'Test Description'
     )
   end
 
-  context 'value not set' do
-    let(:value) { nil }
+  describe '#valid?' do
+    subject { @sensor.records.new(value: value) }
 
-    it 'should be invalid' do
-      expect(subject).not_to be_valid
-      expect(subject).to have(1).errors_on(:value)
+    context 'value not set' do
+      let(:value) { nil }
+
+      it 'should be invalid' do
+        expect(subject).not_to be_valid
+        expect(subject).to have(1).errors_on(:value)
+      end
+    end
+
+    context 'value set to non number' do
+      let(:value) { 'Not a number' }
+
+      it 'should cast the value' do
+        expect(subject.value).to eq('Not a number'.to_f)
+      end
+    end
+
+    context 'value set to number type' do
+      let(:value) { 10.0 }
+
+      it 'it should be valid' do
+        expect(subject).to be_valid
+      end
     end
   end
 
-  context 'value set to non number' do
-    let(:value) { 'Not a number' }
+  describe '.avg_on_day' do
+    context 'only records of granularity finest exist' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,1,2,2,2,'+0'))
+      end
 
-    it 'should cast the value' do
-      expect(subject.value).to eq('Not a number'.to_f)
+      it 'calculates the average of a day' do
+        avg = Record.avg_on_day DateTime.new(2001, 1, 1)
+        expect(avg).to eq 5
+      end
+    end
+
+    context 'records of different days' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,2,1,1,1,'+0'))
+      end
+
+      it 'only calculates the average for records of the same day' do
+        avg = Record.avg_on_day DateTime.new(2001, 1, 1)
+        expect(avg).to eq 3
+      end
+    end
+
+    context 'records of various granularities exist' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1), granularity: Record.granularities[:daily])
+        @sensor.records.create(value: 1.0, created_at: DateTime.new(2001,1,1), granularity: Record.granularities[:montly])
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,1,2,2,2,'+0'))
+      end
+
+      it 'ignores any values other than granularity finest' do
+        avg = Record.avg_on_day DateTime.new(2001, 1, 1)
+        expect(avg).to eq 7
+      end
     end
   end
 
-  context 'value set to number type' do
-    let(:value) { 10.0 }
+  describe '.avg_in_month' do
+    context 'only records of granularity finest exist' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,2,2,2,2,'+0'))
+      end
 
-    it 'it should be valid' do
-      expect(subject).to be_valid
+      it 'calculates the average of a day' do
+        avg = Record.avg_in_month DateTime.new(2001, 1, 1)
+        expect(avg).to eq 5
+      end
+    end
+
+    context 'records of different months' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,2,1,1,1,1,'+0'))
+      end
+
+      it 'only calculates the average for records of the same month' do
+        avg = Record.avg_in_month DateTime.new(2001, 1, 1)
+        expect(avg).to eq 3
+      end
+    end
+
+    context 'records of various granularities exist' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1), granularity: Record.granularities[:daily])
+        @sensor.records.create(value: 1.0, created_at: DateTime.new(2001,1,1), granularity: Record.granularities[:montly])
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,1,2,2,2,'+0'))
+      end
+
+      it 'ignores any values other than granularity finest' do
+        avg = Record.avg_in_month DateTime.new(2001, 1, 1)
+        expect(avg).to eq 7
+      end
+    end
+  end
+
+  describe '.avg_in_year' do
+    context 'only records of granularity finest exist' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,2,2,2,2,'+0'))
+      end
+
+      it 'calculates the average of a day' do
+        avg = Record.avg_in_year DateTime.new(2001, 1, 1)
+        expect(avg).to eq 5
+      end
+    end
+
+    context 'records of different years' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2002,1,1,1,1,1,'+0'))
+      end
+
+      it 'only calculates the average for records of the same month' do
+        avg = Record.avg_in_year DateTime.new(2001, 1, 1)
+        expect(avg).to eq 3
+      end
+    end
+
+    context 'records of various granularities exist' do
+      before :each do
+        @sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1), granularity: Record.granularities[:daily])
+        @sensor.records.create(value: 1.0, created_at: DateTime.new(2001,1,1), granularity: Record.granularities[:montly])
+        @sensor.records.create(value: 7.0, created_at: DateTime.new(2001,1,1,2,2,2,'+0'))
+      end
+
+      it 'ignores any values other than granularity finest' do
+        avg = Record.avg_in_year DateTime.new(2001, 1, 1)
+        expect(avg).to eq 7
+      end
     end
   end
 end
-
