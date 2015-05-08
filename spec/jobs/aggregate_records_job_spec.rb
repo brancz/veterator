7b92932a8917d01083ca767ee2ee201ba9324f4f
flower-pot <fbranczyk@gmail.com>
2015-05-08 18:40:28 +0200

    aggregate finest records to daily records

--- spec/jobs/aggregate_records_job_spec.rb
+++ spec/jobs/aggregate_records_job_spec.rb
@@ -1,3 +1,12 @@
 RSpec.describe AggregateRecordsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'aggregates values by days' do
+    sensor = Sensor.create(title: 'test', description: 'test')
+    sensor.records.create(value: 1.0, created_at: DateTime.new(2001,1,1,1,1,1,'+0'))
+    sensor.records.create(value: 3.0, created_at: DateTime.new(2001,1,1,2,2,2,'+0'))
+
+    AggregateRecordsJob.perform_now
+
+    record = sensor.records.daily.first
+    expect(record.value).to eq 2.0
+  end
 end
